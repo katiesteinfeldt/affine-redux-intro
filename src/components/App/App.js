b2f60f09b969e04d57b3135fc12998ccc28a8f30
Katie Mangan <katie.mangan1@gmail.com>
2019-02-26 15:34:23 -0600

    count state up by one on click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         Hello Redux
-        <button onClick={() => {this.props.dispatch({type: 'BUTTON_ONE'})}}>Button One</button>
+        <button onClick={() => {this.props.dispatch({type: 'BUTTON_ONE'})}}> Button One </button>
+        <button onClick={() => {this.props.dispatch({type: 'BUTTON_TWO'})}}> Button Two </button>
+        <button onClick={() => { this.props.dispatch({ type: 'ADD_ELEMENT' })}}> Button Three </button>
+        <div>
+          <pre>
+            {JSON.stringify(this.props, null, 2)}
+          </pre>
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapReduxStateToProps = (reduxState) => {
+  return reduxState;
+}
+
+export default connect(mapReduxStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
+let count = 0;
+const firstReducer = (state, action) => {
+    if (action.type === 'BUTTON_ONE') {
+        console.log('Button One was clicked');
+        console.log(state);//whatever we return from our reducer, we are adding to our redux state or whatever you return from the first time is what your state is going to be the next time
+        count++;
+    }
+    return count;
+};
+
+const secondReducer = (state, action) => {
+    if (action.type === 'BUTTON_TWO'){
+        console.log(`Button Two was clicked`); 
+        console.log(state);
+    }
+    
+    return 100;
+}
+
+const elementListReducer = (state, action) => {
+    if (action.type === 'ADD_ELEMENT') {
+        console.log(`Add Element was clicked`);
+    }
+    return {};
+}
+
 const storeInstance = createStore(
-    () => {
-        console.log(`Hey I'm a reducer y'all!`);
-    },
+    combineReducers({
+        firstReducer,
+        secondReducer,
+        elementListReducer,
+    }),
 );
 
 
